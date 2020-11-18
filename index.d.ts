31a6bb66dab8c53440d1a7c66aabb0a0129eef62
Bsian <chharry321@gmail.com>
2020-11-18 17:25:22 +0000

    Add missing param

--- index.d.ts
+++ index.d.ts
@@ -1569,6 +1569,7 @@ declare namespace Eris {
     webhookEditMessage(
       webhookID: string,
       token: string,
+      messageID: string,
       options: MessageWebhookContent
     ): Promise<Message<GuildTextableChannel>>;
     on: ClientEvents<this>;
